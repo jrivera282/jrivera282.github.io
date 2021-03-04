9f6d30c7cc351aad97eeb090453a8e587f7bc73e
jrivera282 <55336576+jrivera282@users.noreply.github.com>
2021-03-03 22:06:41 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/jrivera282"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My github link
         </a>
       </header>
     </div>
